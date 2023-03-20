cfb9c8be4d161c1efa66b5aed7197c1d90e7b739
VitaNYCH <115541019+VitaNYCH@users.noreply.github.com>
2023-03-20 21:18:00 +0000

    Deploying to gh-pages from @ VitaNYCH/goit-js-hw-11@9285960e3026af7afde99076b0bd264db6886a15 🚀

--- index.7bc5a739.js
+++
@@ -1,2 +0,0 @@
-const o={searchForm:document.querySelector("#search-form")};console.log(o.searchForm);
-//# sourceMappingURL=index.7bc5a739.js.map
--- index.c48fc60e.js
+++
@@ -1,2 +0,0 @@
-!function(){var o={searchForm:document.querySelector("#search-form")};console.log(o.searchForm)}();
-//# sourceMappingURL=index.c48fc60e.js.map
